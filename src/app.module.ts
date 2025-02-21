5f9057dc995f0a8d9f9528f43fcf61c5d54e1120
Elson-jay <elson@eloiacs.com>
2025-02-21 15:22:34 +0530

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -34,7 +34,11 @@ import { UserEntity } from './login/entity/UserEntity';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        url:configService.get('DATABASE_URL'),
+        host: configService.get('DB_HOST') || 'localhost',
+        port: configService.get('DB_PORT') || 5432,
+        username: configService.get('DB_USERNAME') || 'postgres',
+        password: configService.get('DB_PASSWORD') || '1234',
+        database: configService.get('DB_DATABASE')|| 'apptaclone',
         entities: [ProductEntity,PurchaseEntity,SalesEntity,InventoryEntity,CategoryEntity,SubCategoryEntity,VendorEntity,CustomerEntity,
           StatusEntity,SalesItemEntity,PurchaseItemEntity,UserEntity
         ], 
