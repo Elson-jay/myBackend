58b17ec5f46d1ceb99692719791e0eff9d632440
Elson-jay <elson@eloiacs.com>
2025-02-21 15:54:00 +0530

    add

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProductEntity } from './entity/productEntity';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(
+        @InjectRepository(ProductEntity)
+        private readonly productRepository:Repository<ProductEntity>,
+    ){}
+
+    async getProduct():Promise<ProductEntity[]>{
+        return await this.productRepository.find();
+    }
+}
