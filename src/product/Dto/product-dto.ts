308f78de1894b979a504e8a2ed3a74eb100b640f
Elson-jay <elson@eloiacs.com>
2025-02-22 11:57:16 +0530

    add

--- src/product/Dto/product-dto.ts
+++ src/product/Dto/product-dto.ts
@@ -1,27 +1,35 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class ProductDto {
 
     @IsNotEmpty()
     @IsString()
+    @ApiProperty()
     productName:string;
 
     @IsNotEmpty()
     @IsNumber()
+    @ApiProperty()
     productPrice:number;
 
     @IsNotEmpty()
+    @ApiProperty()
     salesPrice:number;
 
     @IsNotEmpty()
+    @ApiProperty()
     mrp:number;
 
     @IsNotEmpty()
+    @ApiProperty()
     catgoryId:number;
 
     @IsNotEmpty()
+    @ApiProperty()
     subCatgoryId:number;
 
     @IsNotEmpty()
+    @ApiProperty()
     productimg:string;
 }
