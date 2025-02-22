51f237ca1a5503d7fe4819b852f854f9e06a4a14
Elson-jay <elson@eloiacs.com>
2025-02-22 13:18:36 +0530

    add categoryentity in product

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,9 +3,10 @@ import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from './entity/productEntity';
+import { CategoryEntity } from 'src/category/entity/CategoryEntity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([ProductEntity])],
+  imports:[TypeOrmModule.forFeature([ProductEntity,CategoryEntity])],
   controllers: [ProductController],
   providers: [ProductService]
 })
