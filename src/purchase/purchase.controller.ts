139e60f641f8535e4bb4f9d2f554605d6dc9f8eb
Elson-jay <elson@eloiacs.com>
2025-02-22 16:33:10 +0530

    add purchase

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { PurchaseService } from './purchase.service';
+import { PurchaseDto } from './Dto/PurchaseDto';
 
 @Controller('purchase')
-export class PurchaseController {}
+export class PurchaseController {
+    constructor(
+        private readonly purchaseServer:PurchaseService,
+    ){}
+
+    @Post("/addPurchase")
+    async addPurchase(purchaseDto:PurchaseDto){
+        return await this.purchaseServer.addPurchase(purchaseDto)
+    }
+}
